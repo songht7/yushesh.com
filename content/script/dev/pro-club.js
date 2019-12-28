e3f0fa53fce6898777d245808807dac4e94c4db4
songht7 <leon.ting@okkuaixiu.com>
2019-12-28 19:17:53 +0800

    xxx

---
+++ content/script/dev/pro-club.js
@@ -0,0 +1,50 @@
+module.exports = {
+    pro: [{
+        "id": 14,
+        "ctg": 2,
+        "ctgName": "会所售楼处",
+        "title": "芙蕖茶苑会所",
+        "design": "李宏升",
+        "designCom": "御舍设计",
+        "location": "江西九江",
+        "construction": "御舍（上海）建设工程有限公司",
+        "area": "1500㎡",
+        "txtPlace": 5,
+        "textRlign": "right",
+        "showType": "type_a",
+        "cover": [{
+            "url": "c1.jpg",
+            "size": "420x440",
+            "left": "0",
+            "top": "0"
+        }, {
+            "url": "c2.jpg",
+            "size": "860x210",
+            "left": "440",
+            "top": "0"
+        }, {
+            "url": "c3.jpg",
+            "size": "860x210",
+            "left": "440",
+            "top": "230"
+        }, {
+            "url": "c4.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "460"
+        }, {
+            "url": "c5.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "690"
+        }, {
+            "url": "c6.jpg",
+            "size": "420x440",
+            "left": "880",
+            "top": "460"
+        }],
+        "designConcept": "",
+        "content": "",
+        "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"]
+    }]
+};
---
+++ content/script/lib/pro-office.js
@@ -0,0 +1,57 @@
+module.exports = {
+  pro: [{
+    "id": 13,
+    "ctg": 1,
+    "ctgName": "办公空间",
+    "title": "上海谨道电商办公室",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "location": "中国上海",
+    "area": "上海市長寧區長寧來福士辦公樓1A22F",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "1000㎡",
+    "style": "",
+    "txtPlace": 7,
+    "textRlign": "right",
+    "showType": "type_a",
+    "cover": [{
+      "url": "c1.jpg",
+      "size": "420x440",
+      "left": "0",
+      "top": "0"
+    }, {
+      "url": "c2.jpg",
+      "size": "420x210",
+      "left": "440",
+      "top": "0"
+    }, {
+      "url": "c3.jpg",
+      "size": "420x670",
+      "left": "880",
+      "top": "0"
+    }, {
+      "url": "c4.jpg",
+      "size": "420x210",
+      "left": "440",
+      "top": "230"
+    }, {
+      "url": "c5.jpg",
+      "size": "860x210",
+      "left": "0",
+      "top": "460"
+    }, {
+      "url": "c6.jpg",
+      "size": "420x210",
+      "left": "0",
+      "top": "690"
+    }, {
+      "url": "c7.jpg",
+      "size": "860x210",
+      "left": "440",
+      "top": "690"
+    }],
+    "designConcept": "以象形文字中品字的形字以馬的蹄印演化而來，而馬的精神正是品優，品質、品格，代表著公司的三位股東精神，及企業形象",
+    "content": "",
+    "dtlImgs": ["1.jpg", "2.jpg", "3.png", "4.jpg", "5.jpg", "6.jpg", "7.png", "8.png"]
+  }]
+};
