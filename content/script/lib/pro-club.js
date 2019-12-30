b64f0b89e3d28f664c5216430a312ec2de693638
Leon Song <leon.ting@okkuaixiu.com>
2019-12-30 14:55:06 +0800

    xxx

--- content/script/lib/pro-club.js
+++ content/script/lib/pro-club.js
@@ -100,5 +100,78 @@ module.exports = {
     "content": "",
     "dtlImgType": "full",
     "dtlImgs": ["1.jpg", "2.jpg", "3.jpg"]
+  }, {
+    "id": 38,
+    "ctg": 2,
+    "ctgName": "会所售楼处",
+    "title": "北京当代置业",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "location": "中国北京",
+    "area": "",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "2000㎡",
+    "txtPlace": 5,
+    "textRlign": "right",
+    "showType": "type_a",
+    "cover": [{
+      "url": "c1.jpg",
+      "size": "420x440",
+      "left": "0",
+      "top": "0"
+    }, {
+      "url": "c2.jpg",
+      "size": "420x440",
+      "left": "440",
+      "top": "0"
+    }, {
+      "url": "c3.jpg",
+      "size": "420x440",
+      "left": "880",
+      "top": "0"
+    }, {
+      "url": "c4.jpg",
+      "size": "860x210",
+      "left": "0",
+      "top": "460"
+    }, {
+      "url": "c5.jpg",
+      "size": "420x210",
+      "left": "880",
+      "top": "460"
+    }, {
+      "url": "c6.jpg",
+      "size": "420x210",
+      "left": "0",
+      "top": "690"
+    }, {
+      "url": "c7.jpg",
+      "size": "860x210",
+      "left": "440",
+      "top": "690"
+    }],
+    "designConcept": "",
+    "content": "",
+    "dtlImgType": "full",
+    "dtlImgs": ["1.jpg", "2.jpg"]
+  }, {
+    "id": 39,
+    "ctg": 2,
+    "ctgName": "会所售楼处",
+    "title": "常州中海龙城公馆",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "location": "江苏常州",
+    "area": "",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "1800㎡",
+    "txtPlace": 1,
+    "textRlign": "",
+    "showType": "single",
+    "cover": "c1.jpg",
+    "designConcept": "",
+    "content": "",
+    "dtlImgType": "full",
+    "dtlImgs": ["1.jpg", "2.jpg", "3.jpg"]
   }]
 };
