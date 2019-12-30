70c29bc04083b8981842c75fc58e385239d006cd
Leon Song <leon.ting@okkuaixiu.com>
2019-12-30 13:36:54 +0800

    xxx

--- content/script/dev/pro-office.js
+++ content/script/dev/pro-office.js
@@ -65,7 +65,7 @@ module.exports = {
         "construction": "御舍（上海）建设工程有限公司",
         "area": "7000㎡",
         "style": "",
-        "txtPlace": 7,
+        "txtPlace": 6,
         "textRlign": "right",
         "showType": "type_a",
         "cover": [{
@@ -102,5 +102,60 @@ module.exports = {
         "designConcept": "",
         "content": "",
         "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.jpg", "39.jpg", "40.jpg", "41.jpg", "42.jpg", "43.jpg", "44.jpg"]
+    },{
+        "id": 35,
+        "ctg": 1,
+        "ctgName": "办公空间",
+        "title": "金富地产",
+        "design": "李宏升",
+        "designCom": "御舍设计",
+        "location": "江苏张家港",
+        "area": "",
+        "construction": "御舍（上海）建设工程有限公司",
+        "area": "600㎡",
+        "style": "",
+        "txtPlace": 7,
+        "textRlign": "right",
+        "showType": "type_a",
+        "cover": [{
+            "url": "c1.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "0"
+        }, {
+            "url": "c2.jpg",
+            "size": "420x210",
+            "left": "880",
+            "top": "0"
+        }, {
+            "url": "c3.jpg",
+            "size": "420x440",
+            "left": "0",
+            "top": "230"
+        }, {
+            "url": "c4.jpg",
+            "size": "420x440",
+            "left": "440",
+            "top": "230"
+        }, {
+            "url": "c5.jpg",
+            "size": "420x440",
+            "left": "880",
+            "top": "230"
+        }, {
+            "url": "c6.jpg",
+            "size": "420x210",
+            "left": "0",
+            "top": "690"
+        }, {
+            "url": "c7.jpg",
+            "size": "860x210",
+            "left": "440",
+            "top": "690"
+        }],
+        "designConcept": "",
+        "content": "",
+        "dtlImgType": "full",
+        "dtlImgs": ["1.jpg", "2.jpg", "3.jpg"]
     }]
 };
