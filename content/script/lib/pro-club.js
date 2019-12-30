7c6ef094d84d3374cf51f8d4ac4e142faa9924f3
Leon Song <leon.ting@okkuaixiu.com>
2019-12-30 15:21:32 +0800

    xxx

--- content/script/lib/pro-club.js
+++ content/script/lib/pro-club.js
@@ -184,7 +184,7 @@ module.exports = {
     "area": "",
     "construction": "御舍（上海）建设工程有限公司",
     "area": "2000㎡",
-    "txtPlace": 5,
+    "txtPlace": 9,
     "textRlign": "right",
     "showType": "type_a",
     "cover": [{
@@ -237,5 +237,59 @@ module.exports = {
     "content": "",
     "dtlImgType": "",
     "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"]
+  }, {
+    "id": 41,
+    "ctg": 2,
+    "ctgName": "会所售楼处",
+    "title": "南京融信G58售楼处",
+    "design": "李宏升",
+    "designCom": "御舍设计",
+    "location": "江苏南京",
+    "area": "",
+    "construction": "御舍（上海）建设工程有限公司",
+    "area": "1600㎡",
+    "txtPlace": 7,
+    "textRlign": "right",
+    "showType": "type_a",
+    "cover": [{
+      "url": "c1.jpg",
+      "size": "860x210",
+      "left": "0",
+      "top": "0"
+    }, {
+      "url": "c2.jpg",
+      "size": "420x210",
+      "left": "880",
+      "top": "0"
+    }, {
+      "url": "c3.jpg",
+      "size": "420x210",
+      "left": "0",
+      "top": "230"
+    }, {
+      "url": "c4.jpg",
+      "size": "860x210",
+      "left": "440",
+      "top": "230"
+    }, {
+      "url": "c5.jpg",
+      "size": "420x440",
+      "left": "0",
+      "top": "460"
+    }, {
+      "url": "c6.jpg",
+      "size": "420x440",
+      "left": "440",
+      "top": "460"
+    }, {
+      "url": "c7.jpg",
+      "size": "420x440",
+      "left": "880",
+      "top": "460"
+    }],
+    "designConcept": "",
+    "content": "",
+    "dtlImgType": "full",
+    "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg"]
   }]
 };
