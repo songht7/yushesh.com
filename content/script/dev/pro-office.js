df91655f43411b4573d37ec79befbab8a5cdb7df
Leon Song <leon.ting@okkuaixiu.com>
2019-12-30 13:14:01 +0800

    xxx

--- content/script/dev/pro-office.js
+++ content/script/dev/pro-office.js
@@ -53,5 +53,54 @@ module.exports = {
         "designConcept": "以象形文字中品字的形字以馬的蹄印演化而來，而馬的精神正是品優，品質、品格，代表著公司的三位股東精神，及企業形象",
         "content": "",
         "dtlImgs": ["1.jpg", "2.jpg", "3.png", "4.jpg", "5.jpg", "6.jpg", "7.png", "8.png"]
+    },{
+        "id": 34,
+        "ctg": 1,
+        "ctgName": "办公空间",
+        "title": "安徽楚江集团",
+        "design": "陈婷婷",
+        "designCom": "御舍设计",
+        "location": "中国安徽",
+        "area": "",
+        "construction": "御舍（上海）建设工程有限公司",
+        "area": "7000㎡",
+        "style": "",
+        "txtPlace": 7,
+        "textRlign": "right",
+        "showType": "type_a",
+        "cover": [{
+            "url": "c1.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "0"
+        }, {
+            "url": "c2.jpg",
+            "size": "420x440",
+            "left": "880",
+            "top": "0"
+        }, {
+            "url": "c3.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "230"
+        }, {
+            "url": "c4.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "460"
+        }, {
+            "url": "c5.jpg",
+            "size": "420x440",
+            "left": "880",
+            "top": "460"
+        }, {
+            "url": "c6.jpg",
+            "size": "860x210",
+            "left": "0",
+            "top": "690"
+        }],
+        "designConcept": "",
+        "content": "",
+        "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg", "37.jpg", "38.jpg", "39.jpg", "40.jpg", "41.jpg", "42.jpg", "43.jpg", "44.jpg"]
     }]
 };
