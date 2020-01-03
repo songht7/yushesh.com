f86f345b25ced3af570dde52a32673c8c3477125
Leon Song <leon.ting@okkuaixiu.com>
2020-01-03 11:14:41 +0800

    xxx

--- content/script/lib/pro-office.js
+++ content/script/lib/pro-office.js
@@ -209,6 +209,6 @@ module.exports = {
     "designConcept": "",
     "content": "",
     "dtlImgType": "",
-    "dtlImgs": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"]
+    "dtlImgs": ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"]
   }]
 };
