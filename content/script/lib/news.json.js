ce26f98bf0d14de419ecdd24e85b4b85c928bbe3
songht7 <407794660@qq.com>
2020-05-17 18:20:13 +0800

    news

--- content/script/lib/news.json.js
+++ content/script/lib/news.json.js
@@ -9,5 +9,45 @@ module.exports = {
     title: "御舍&HOUSE OF ROHL",
     date: "2020年5月7日",
     cover: "n2.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/-LLUz2BgFo25Wn2w4mmE7g",
+    title: "劳动节",
+    date: "2020年5月1日",
+    cover: "n3.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/hI_iWRWfIG5CzrvNOcmnHw",
+    title: "御舍&VERSACE",
+    date: "2020年4月24日",
+    cover: "n4.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/hI_iWRWfIG5CzrvNOcmnHw",
+    title: "快讯 | 御舍设计龍吟日料软装设计荣获I-DING设计大奖2019",
+    date: "2020年4月8日",
+    cover: "n5.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/Y3c9FnjvFOrvOKOHDrjnEg",
+    title: "清明",
+    date: "2020年4月4日",
+    cover: "n6.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/9Be1LyJK4NuhTpqEIKXR_g",
+    title: "快讯 | 恭贺御舍设计庭合景观达成战略合作",
+    date: "2020年3月31日",
+    cover: "n7.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/fy14Ui3SsBY5dK-YI_ZglQ",
+    title: "TOMMY LI 2019作品集回顾",
+    date: "2020年3月25日",
+    cover: "n8.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/PF13ZLyYBNs3p3HknoOK_g",
+    title: "快讯 | 御舍设计龍吟日料 荣获2019国际设计大奖-艾鼎奖",
+    date: "2020年3月24日",
+    cover: "n9.jpg"
+  }, {
+    link: "https://mp.weixin.qq.com/s/ilINmH3h14oRaIlOIzpBzw",
+    title: "以创新，致未来|御舍设计TOMMY LI回顾2019",
+    date: "2020年3月11日",
+    cover: "n10.jpg"
   }]
 };
